d3d7a0e1e27f784729d611b798b4cb8254390f79
gopal <gopaldk@gmail.com>
2018-02-19 23:01:14 +0530

    InventoryService test cases

---
+++ inventory.dashboard.app/src/test/java/com/example/demo/InventoryServiceTest.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InventoryServiceTest {
+
+	@Test
+	public void getVehicles() {
+		assertEquals(20, InvetoryService.getVehicles().size());
+	}
+
+	@Test
+	public void getCustomers() {
+		assertEquals(5, InvetoryService.getCustomers("CORPORATE").size());
+	}
+
+	@Test
+	public void getRecentTrips() {
+		assertEquals(4, InvetoryService.getRecentTrips(7).size());
+	}
+}
